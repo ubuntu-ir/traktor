240d94779803a4e9f72daca61e412c221c6292c4
Nima Fatemi <mrphs@riseup.net>
2016-10-07 17:56:47 +0000

    add tor repo to sources.list

--- traktor.sh
+++ traktor.sh
@@ -11,6 +11,25 @@ echo "UseBridges 1
 Bridge obfs4 194.132.209.170:36441 B16B4B1B10910B6EC4A3E713297C4EAE9DFB5229 cert=SzdrMUoL49NrQ0WpTy3dw26MlxNAcvD3lLFqZDrAA/euN++77WueeirzoV2OU5QpJplfUQ iat-mode=0
 ClientTransportPlugin obfs4 exec /usr/bin/obfs4proxy" | sudo tee /etc/tor/torrc > /dev/null
 
+# Reload Tor for new torrc to take effect
+sudo service tor reload
+
+# Get dist info
+DIST=$(lsb_release -sc)
+
+# Add Tor to sources.list to get the latest version
+sudo printf \
+      "deb http://deb.torproject.org/torproject.org $DIST main \
+      \ndeb-src http://deb.torproject.org/torproject.org $DIST main" \
+      >> /etc/apt/sources.list.d/tor.list
+
+# Fetching Tor signing key and adding it to the keyring
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+# Update Tor
+sudo torsocks apt update && sudo apt install tor -y
+
 # Fix Problem Apparmor
 sudo sed -i '27s/PUx/ix/' /etc/apparmor.d/abstractions/tor
 sudo apparmor_parser -r -v /etc/apparmor.d/system_tor
@@ -26,9 +45,6 @@ sudo service polipo restart
 gsettings set org.gnome.system.proxy mode 'manual'
 gsettings set org.gnome.system.proxy.http host 127.0.0.1
 gsettings set org.gnome.system.proxy.http port 8123
-
-# Restart Tor Service
-sudo service tor restart
 
 # Install Finish
 echo "Install Finished successfully…"
