ce0897c5e0f6b50f0a9ad0b5257ae950d4212160
amin0256 <amin3mej@gmail.com>
2016-11-18 17:45:05 +0330

    fix issue #18

--- traktor.sh
+++ traktor.sh
@@ -1,14 +1,22 @@
-#!/bin/bash 
+#!/bin/bash
 
 # License : GPLv3+
 
 #Checking if the distro is debianbase / archbase  and running the correct script
 if pacman -Q &> /dev/null ;then
+  if [ ! -f ./traktor_arch.sh ]; then
+    wget -O ./traktor_arch.sh 'https://raw.githubusercontent.com/ubuntu-ir/traktor/master/traktor_arch.sh' || curl -O  https://raw.githubusercontent.com/ubuntu-ir/traktor/master/traktor_arch.sh
+  fi
+  sudo chmod +x ./traktor_arch.sh
  ./traktor_arch.sh
-#    echo "arch"
+  # echo "arch"
 elif apt list --installed &> /dev/null ;then
-   ./traktor_debian.sh
-#    echo "debian"
+  if [ ! -f ./traktor_debian.sh ]; then
+    wget -O ./traktor_debian.sh 'https://raw.githubusercontent.com/ubuntu-ir/traktor/master/traktor_debian.sh' || curl -O  https://raw.githubusercontent.com/ubuntu-ir/traktor/master/traktor_debian.sh
+  fi
+  sudo chmod +x ./traktor_debian.sh
+  ./traktor_debian.sh
+  # echo "debian"
 else
     echo "Your distro is neither archbase nor debianbase So, The script is not going to work in your distro."
-fi  
+fi
