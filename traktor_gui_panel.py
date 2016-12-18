9ef42864f629b5dec92a01a5775ba8390e838c2f
Danial Behzadi <dani.behzi@ubuntu.com>
2016-12-18 13:27:13 +0330

    enhance indicator

--- traktor_gui_panel.py
+++ traktor_gui_panel.py
@@ -8,40 +8,27 @@ gi.require_version('Gtk','3.0')
 gi.require_version('AppIndicator3','0.1')
 gi.require_version('Notify','0.7')
 
-from gi.repository import Gtk as gtk
+from gi.repository import Gtk, Gio, Notify
 from gi.repository import AppIndicator3 as appindicator
-from gi.repository import Notify as notify
+from gettext import gettext as T
 
-from os import popen as ___
+from os import popen #TODO remove dependency
 
 APPINDICATOR_ID = 'traktor'
+Name = T("Traktor")
 
-#Commands
-GSETTINGS_SET_PROXY_SCHEMA = 'gsettings set org.gnome.system.proxy mode '
-GSETTINGS_GET_PROXY_SCHEMA = 'gsettings get org.gnome.system.proxy mode '
+#Commands #TODO remove bash commands
 RESTART_TOR = 'sudo systemctl restart tor.service'
-RESTART_POLIPO = 'service polipo restart'
 
-#Proxy Options (gsettings values)
-PROXY_VALUE_NONE = 'none'
-PROXY_VALUE_MANUAL = 'manual'
-
-#Messages
-MSG_TOR_RELOAD_SUCCESS = "TOR Reloaded Successfully"
-MSG_TOR_ACTIVE_SUCCESS = "TOR Activated Successfully"
-MSG_SWITCH_TO_NORMAL = "TOR Switched To Normal Mode Successfully"
-MSG_FAILUERE = "Something Went Wrong !\n"
-
-#Current Working Directory
+#Current Working Directory #TODO Move to /usr/share/traktor/icons
 cwd = os.path.abspath(os.path.dirname(__file__))
 
 #Icon pathes
-ICON_TOR_ON = os.path.join(cwd, 'traktor_gui_panel/photos/tor_proxy_mode.svg')
-ICON_TOR_OFF = os.path.join(cwd, 'traktor_gui_panel/photos/tor_normal_mode.svg')
+ICON_TOR_ON = os.path.join(cwd, 'traktor_gui_panel/icons/tor_proxy_mode.svg')
+ICON_TOR_OFF = os.path.join(cwd, 'traktor_gui_panel/icons/tor_normal_mode.svg')
 
-
-
-if PROXY_VALUE_MANUAL in ___(GSETTINGS_GET_PROXY_SCHEMA).read():
+proxy = Gio.Settings.new("org.gnome.system.proxy")
+if (Gio.Settings.get_string(proxy, "mode")=="manual"):
     indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath(ICON_TOR_ON),appindicator.IndicatorCategory.SYSTEM_SERVICES)
 else:
     indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath(ICON_TOR_OFF),appindicator.IndicatorCategory.SYSTEM_SERVICES)
@@ -49,58 +36,53 @@ else:
 def main(indicator):
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
-    notify.init(APPINDICATOR_ID)
-    gtk.main()
+    Notify.init(APPINDICATOR_ID)
+    Gtk.main()
 
 def build_menu():
-    menu = gtk.Menu()
-    item_nm = gtk.MenuItem('Disable Proxy')
-    item_nm.connect('activate', nm)
-    item_np = gtk.MenuItem('Enable Proxy')
-    item_np.connect('activate', np)
-    item_rl = gtk.MenuItem('Restart Tor & Polipo')
-    item_rl.connect('activate', rl)
-    item_quit = gtk.MenuItem('Quit')
+    menu = Gtk.Menu()
+    item_normal = Gtk.MenuItem('Disable Proxy')
+    item_normal.connect('activate', normal_mode)
+    item_proxy = Gtk.MenuItem('Enable Proxy')
+    item_proxy.connect('activate', proxy_mode)
+    item_start = Gtk.MenuItem('Restart Tor')
+    item_start.connect('activate', restart)
+    item_quit = Gtk.MenuItem('Quit')
     item_quit.connect('activate', quit)
-    menu.append(item_nm)
-    menu.append(item_np)
-    menu.append(item_rl)
+    menu.append(item_normal)
+    menu.append(item_proxy)
+    menu.append(item_start)
     menu.append(item_quit)
     menu.show_all()
     return menu
 
+def normal_mode(_):
+    proxy.set_string("mode", "none")
+    indicator.set_icon(str(os.path.abspath(ICON_TOR_OFF)))
+    Notify.Notification.new("<b>"+Name+"</b>", T("Switched to normal mode"), None).show()
 
-def fetch(proxy_value, icon_path, msg1):
-    msg = ___(GSETTINGS_SET_PROXY_SCHEMA + proxy_value + ";" + RESTART_TOR +"&&" + RESTART_POLIPO).read()
+def proxy_mode(_):
+    proxy.set_string("mode", "manual")
+    proxy.set_strv("ignore-hosts", ['localhost', '127.0.0.0/8', '::1', '192.168.0.0/16', '10.0.0.0/8', '172.16.0.0/12'])
+    http = Gio.Settings.new("org.gnome.system.proxy.http")
+    http.set_string("host", "127.0.0.1")
+    http.set_int("port", 8123)
+    socks = Gio.Settings.new("org.gnome.system.proxy.socks")
+    socks.set_string("host", "127.0.0.1")
+    socks.set_int("port", 9050)
+    indicator.set_icon(str(os.path.abspath(ICON_TOR_ON)))
+    Notify.Notification.new("<b>"+Name+"</b>", T("Tor activated"), None).show()
+
+def restart(_):
+    msg = popen(RESTART_TOR).read() #TODO write in pure python
     if msg == "":
-        indicator.set_icon(str(os.path.abspath(icon_path)))
-        return msg1
+        Notify.Notification.new("<b>"+Name+"</b>", T("Tor restarted"), None).show()
     else:
-        return MSG_FAILUERE + msg
-
-
-def fetch_normal_mode():
-    return fetch(PROXY_VALUE_NONE, ICON_TOR_OFF, MSG_SWITCH_TO_NORMAL)
-
-def fetch_proxy_mode():
-    return fetch(PROXY_VALUE_MANUAL, ICON_TOR_ON, MSG_TOR_ACTIVE_SUCCESS)
-
-def fetch_reload():
-    return fetch(PROXY_VALUE_MANUAL, ICON_TOR_ON, MSG_TOR_RELOAD_SUCCESS )
-
-
-def nm(_):
-    notify.Notification.new("<b>Status</b>", fetch_normal_mode(), None).show()
-
-def np(_):
-    notify.Notification.new("<b>Status</b>", fetch_proxy_mode(), None).show()
-
-def rl(_):
-    notify.Notification.new("<b>Status</b>", fetch_reload(), None).show()
+        Notify.Notification.new("<b>"+Name+"</b>", T("Something went wrong. See the log"), None).show()
 
 def quit(_):
-    notify.uninit()
-    gtk.main_quit()
+    Notify.uninit()
+    Gtk.main_quit()
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
