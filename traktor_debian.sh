ee9bf2ab9f8d811e8ec820c33de22fb4481c3d02
Sosha Mosleh <sosha.m.1996@gmail.com>
2016-12-25 04:49:46 +0330

    Update traktor_debian.sh

--- traktor_debian.sh
+++ traktor_debian.sh
@@ -68,8 +68,9 @@ sudo sed -i '27s/PUx/ix/' /etc/apparmor.d/abstractions/tor
 sudo apparmor_parser -r -v /etc/apparmor.d/system_tor
 
 # Traktor GUI Panel 
-mkdir $HOME/.traktor_gui_panel mv traktor_gui_panel.py $HOME/.traktor_gui_panel 
-mv photos $HOME/.traktor_gui_panel/photos 
+mkdir $HOME/.traktor_gui_panel
+mv traktor-master/traktor_gui_panel.py $HOME/.traktor_gui_panel 
+mv traktor_gui_panel/icons $HOME/.traktor_gui_panel/
 
 sudo touch /usr/share/applications/traktor-gui-panel.desktop 
 echo "[Desktop Entry]
@@ -83,7 +84,7 @@ Comment[fa]=تراکتور پنل گرافیکی
 Exec=$HOME/.traktor_gui_panel/traktor_gui_panel.py
 Terminal=false Type=Application
 Categories=Network;Application;
-Icon=$HOME/.traktor_gui_panel/photos/tor.png
+Icon=$HOME/.traktor_gui_panel/icons/tor.png
 Keywords=Tor;Browser;Proxy;VPN;Internet;Web" | sudo tee /usr/share/applications/traktor-gui-panel.desktop > /dev/null
 
 # update finished
