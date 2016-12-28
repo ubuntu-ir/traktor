60cab3aec4d17c593ec8f9db05fe6beeca902d99
Sosha Mosleh <sosha.m.1996@gmail.com>
2016-12-29 00:40:46 +0330

    Fix traktor-gui-panel.desktop

--- traktor_debian.sh
+++ traktor_debian.sh
@@ -71,21 +71,23 @@ sudo apparmor_parser -r -v /etc/apparmor.d/system_tor
 mkdir $HOME/.traktor_gui_panel
 mv traktor_gui_panel.py $HOME/.traktor_gui_panel 
 mv traktor_gui_panel/icons $HOME/.traktor_gui_panel/
+chmod +x ~/.traktor_gui_panel/traktor_gui_panel.py
 
 sudo touch /usr/share/applications/traktor-gui-panel.desktop 
 echo "[Desktop Entry]
 Version=1.0
 Name=Traktor GUI Panel
 Name[fa]=تراکتور پنل گرافیکی
-GenericName:Traktor Panel
+GenericName=Traktor Panel
 GenericName[fa]=تراکتور پنل
 Comment=Traktor GUI Panel
 Comment[fa]=تراکتور پنل گرافیکی
 Exec=$HOME/.traktor_gui_panel/traktor_gui_panel.py
-Terminal=false Type=Application
+Terminal=false
+Type=Application
 Categories=Network;Application;
-Icon=$HOME/.traktor_gui_panel/icons/tor.png
-Keywords=Tor;Browser;Proxy;VPN;Internet;Web" | sudo tee /usr/share/applications/traktor-gui-panel.desktop > /dev/null
+Icon=$HOME/.traktor_gui_panel/icons/traktor.png
+Keywords=Tor;Browser;Proxy;VPN;Internet;Web" | sudo tee ~/Desktop/traktor-gui-panel.desktop > /dev/null
 
 # update finished
 echo "Congratulations!!! Your computer is using Tor. may run torbrowser-launcher now."
