435686d272277ac53bdd980ac37645d4a9013a4a
Abtin <shervinmoheban@gmail.com>
2017-08-04 00:47:05 +0430

    Update uninstall_opensuse.sh

--- uninstall_opensuse.sh
+++ uninstall_opensuse.sh
@@ -1,6 +1,6 @@
 clear
 echo -e "Traktor\nTor will be automatically uinstalled ...\n\n"
-sudo zypper rr server_dns server_proxy home:hayyan71
+sudo zypper rr server_dns home:hayyan71
 sudo zypper rm -y obfs4proxy tor torsocks dnscrypt-proxy privoxy 
 sudo rm -f /etc/tor/torrc 
 gsettings set org.gnome.system.proxy mode 'none'
